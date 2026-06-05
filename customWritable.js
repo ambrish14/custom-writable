485cdb50a21f2f3f2b9923152143831d1b0ba60b
Ambrish Chaurasia <69149051+ambrish14@users.noreply.github.com>
2026-06-06 01:17:05 +0530

    "here we using the construct method it will run after the constructr and callback(err) will be executed if err occur it will exit the stream method it will be taken care by Writable method and or else we continue to write "

--- customWritable.js
+++ customWritable.js
@@ -1,7 +1,36 @@
 const { Writable } = require("stream");
+const fs = require("node:fs");
 
 class FileWriteStream extends Writable {
   constructor({ highWaterMark, fileName }) {
     super({ highWaterMark });
+
+    this.fileName = fileName;
+    this.fd = null;
   }
+
+  //this will run after the constructor method and it will  put of the all the other
+  //method until we call the callback function
+  _construct(callback) {
+    fs.open(this.fileName, "w", (err, fd) => {
+      if (err) {
+        callback(err);
+      } else {
+        this.fd = fd;
+        callback();
+      }
+    });
+  }
+  _write(chunk, encoding, callback) {}
+
+  _final() {}
+  _destroy() {}
 }
+
+const stream = new FileWriteStream({ highWaterMark: 1800 });
+
+stream.write(Buffer.from("this is some stream"));
+
+stream.end(Buffer.from("this is last stream "));
+
+stream.on("drain", () => {});
